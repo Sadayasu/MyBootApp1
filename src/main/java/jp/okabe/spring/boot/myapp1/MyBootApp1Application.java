56c5aa43d38661080eba60239fe50c9ae096bb74
umetsu <umetsu@DESKTOP-5UKKBB1>
2015-12-05 15:34:10 +0900

    起動できるようにした

--- src/main/java/jp/okabe/spring/boot/myapp1/MyBootApp1Application.java
+++ src/main/java/jp/okabe/spring/boot/myapp1/MyBootApp1Application.java
@@ -1,9 +1,13 @@
 package jp.okabe.spring.boot.myapp1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class MyBootApp1Application {
 
     public static void main(String[] args) {
