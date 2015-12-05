a4767775be5755b8f528222a781f4b31096165e4
Sadayasu <sadayasu.info@gmail.com>
2015-12-05 15:00:21 +0900

    スプリングproject新規作成 色々ごちゃごちゃ使ってください

---
+++ src/test/java/jp/okabe/spring/boot/myapp1/MyBootApp1ApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.okabe.spring.boot.myapp1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MyBootApp1Application.class)
+public class MyBootApp1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
